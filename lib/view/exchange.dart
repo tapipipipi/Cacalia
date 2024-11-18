407ccdbe07059481e5c626232c4a7909228adf6f
Yu-deeptale <wintinver@gmail.com>
2024-11-18 10:37:53 +0900

    使用する画面の空ファイルの追加

---
+++ lib/view/exchange.dart
@@ -0,0 +1,13 @@
+// 名刺交換画面
+import 'package:flutter/material.dart';
+import 'package:cacalia/view/components/exchange_card.dart';
+
+class Exchange extends StatelessWidget {
+  const Exchange({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const ExchangeCard();
+  }
+}
+
---
+++ lib/view/timeline.dart
@@ -0,0 +1,11 @@
+// 掲示板画面
+import 'package:flutter/material.dart';
+
+class Timeline extends StatelessWidget {
+  const Timeline({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text('Timeline');
+  }
+}
